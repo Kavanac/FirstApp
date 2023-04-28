92d58aa5f350a844e176d666b941470d717204aa
Kavana C <ckavana12@gmail.com>
2023-04-28 13:42:55 +0530

    first commit

---
+++ FirstApp/src/FirstApp.java
@@ -0,0 +1,13 @@
+
+public class FirstApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] a= {10,20,30,40,50};
+		for(int elem:a) {
+			System.out.println(elem);
+		}
+	}
+
+}
